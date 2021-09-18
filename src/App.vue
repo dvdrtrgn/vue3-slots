ac0145f7fa29c92aac6eff20c38191caf577f69d
dvdrtrgn <dvdrtrgn@gmail.com>
2021-09-18 11:59:03 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <app-button id="red" @click="log">
+      <hart-icon />
+      add to favorites
+    </app-button>
+  </div>
+</template>
+
+<script>
+import AppButton from './components/AppButton.vue';
+import HartIcon from './components/HartIcon.vue';
+
+export default {
+  components: {
+    AppButton,
+    HartIcon,
+  },
+  methods: {
+    log() {
+      console.log('works');
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: sans-serif;
+}
+</style>
---
+++ src/components/AppButton.vue
@@ -0,0 +1,33 @@
+<!-- https://vueschool.io/lessons/using-slots -->
+
+<!-- 
+  Adding v-on="$listeners" is not be needed in v3
+  In fact $listeners was removed, as documented here:
+  https://v3.vuejs.org/guide/migration/listeners-removed.html
+-->
+
+<template>
+  <!-- 
+  <button v-on="$listeners"> 
+    <slot></slot>
+  </button>
+  -->
+  <strong>
+    Some text!
+    <button v-bind="$attrs">
+      <slot></slot>
+    </button>
+  </strong>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false, // use false to control who gets attrs
+};
+</script>
+
+<style>
+#red {
+  color: red;
+}
+</style>
